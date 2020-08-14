929a8ff049a16c28bb173c866206f16169eea387
Adriverforlife <Michael@corner-exit.com>
2020-08-14 09:15:28 -0700

    LiferayLogin Answers login question

--- LiferayLogin.js
+++ LiferayLogin.js
@@ -6,8 +6,10 @@ fixture `React Testing`
     const signInStart = Selector('a.sign-in');
     const signInButton = Selector('button.btn.btn-primary > span.lfr-btn-label').withText('Sign In');
     const avatar = Selector('span.user-avatar-link');
-    const avatarDropdown = Selector('div.dropdown-menu')
-    const avatarUserName = Selector('div.dropdown-menu > ul > li')
+    const avatarDropdown = Selector('div.dropdown-menu');
+    const avatarUserName = Selector('div.dropdown-menu > ul > li');
+    const signInQuestion = Selector('#reminderQueryAnswer');
+    const  saveButton = Selector('button > span.lfr-btn-label').withText('Save')
 //Test case equivelant follows
 test('Log In Smoke Test', async t => {
     await t
@@ -15,7 +17,14 @@ test('Log In Smoke Test', async t => {
         .typeText('#_com_liferay_login_web_portlet_LoginPortlet_login', 'test')
         .typeText('#_com_liferay_login_web_portlet_LoginPortlet_password', 'test')
         .click(signInButton())
+        //If first sign-in
+        if(await signInQuestion().exists) {
+           await t 
+                .typeText(signInQuestion(), 'test')
+                .click(saveButton());
+        }
         //Avatar Element will only be present if you are logged in. 
+    await t 
         .click(avatar())
     });
 
