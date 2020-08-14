ec91bdf21b23316988e32a756ccc283938742190
Adriverforlife <Michael@corner-exit.com>
2020-08-14 15:55:02 -0700

    LiferayLogin.js Added clear on login

--- LiferayLogin.js
+++ LiferayLogin.js
@@ -5,17 +5,22 @@ fixture `React Testing`
 //path file equivelants
     const signInStart = Selector('a.sign-in');
     const signInButton = Selector('button.btn.btn-primary > span.lfr-btn-label').withText('Sign In');
+    const userName = Selector('#_com_liferay_login_web_portlet_LoginPortlet_login')
+    const userPassword = Selector('#_com_liferay_login_web_portlet_LoginPortlet_password')
     const avatar = Selector('span.user-avatar-link');
     const avatarDropdown = Selector('div.dropdown-menu');
     const avatarUserName = Selector('div.dropdown-menu > ul > li');
     const signInQuestion = Selector('#reminderQueryAnswer');
     const  saveButton = Selector('button > span.lfr-btn-label').withText('Save')
+    
 //Test case equivelant follows
 test('Log In Smoke Test', async t => {
     await t
         .click(signInStart())
-        .typeText('#_com_liferay_login_web_portlet_LoginPortlet_login', 'test')
-        .typeText('#_com_liferay_login_web_portlet_LoginPortlet_password', 'test')
+        .selectText(userName())
+        .pressKey('delete')
+        .typeText(userName(), 'test@liferay.com')
+        .typeText(userPassword(), 'test')
         .click(signInButton())
         //If first sign-in
         if(await signInQuestion().exists) {
@@ -27,4 +32,3 @@ test('Log In Smoke Test', async t => {
     await t 
         .click(avatar())
     });
-
